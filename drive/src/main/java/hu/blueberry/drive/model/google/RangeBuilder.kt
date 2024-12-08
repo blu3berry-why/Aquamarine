901caf33587dc4ddb25a7aacf6bb5857e1facebd
blu3berry-why <59977342+blu3berry-why@users.noreply.github.com>
2024-12-08 19:49:13 +0100

    RangeBuilder column is now also nullable

--- drive/src/main/java/hu/blueberry/drive/model/google/RangeBuilder.kt
+++ drive/src/main/java/hu/blueberry/drive/model/google/RangeBuilder.kt
@@ -4,39 +4,42 @@ class RangeBuilder(
     private var workSheetName:String
 ) {
     private var start:String = "A1"
-    private var end:String = "A1"
+    private var end:String? = null
 
     constructor(
        workSheetName: String,
        start: String,
-       end:String,
+       end: String?,
     ) : this(workSheetName) {
         this.start = start
         this.end = end
     }
 
-    fun start(colum:String, row:Int? = null): RangeBuilder {
-        start = if (row == null){
-            colum
-        }else{
-            (colum) + row.toString()
+    fun start(colum:String?, row:Int? = null): RangeBuilder {
+        start = when{
+            colum != null && row == null -> colum
+            colum == null && row != null -> row.toString()
+            colum != null && row != null -> (colum) + row.toString()
+            else -> throw IllegalArgumentException("Start cell value can't be empty!")
         }
         return this
     }
 
-    fun end(colum:String, row:Int? = null): RangeBuilder {
-        end = if (row == null){
-            colum
-        }else{
-            (colum) + row.toString()
+    fun end(colum:String?, row:Int? = null): RangeBuilder {
+        end = when{
+            colum != null && row == null -> colum
+            colum == null && row != null -> row.toString()
+            colum != null && row != null -> (colum) + row.toString()
+            else -> null
         }
         return this
     }
-
-
 
 
     fun build():String{
+        if (end == null){
+            return "'$workSheetName'!$start"
+        }
         return "'$workSheetName'!$start:$end"
     }
 
---
+++ drive/src/main/java/hu/blueberry/drive/model/google/enums/RenderOption.kt
@@ -0,0 +1,7 @@
+package hu.blueberry.drive.model.google.enums
+
+enum class RenderOption(val stringValue: String) {
+    FORMATTED_VALUE("FORMATTED_VALUE"),
+    UNFORMATTED_VALUE("UNFORMATTED_VALUE"),
+    FORMULA("FORMULA")
+}
